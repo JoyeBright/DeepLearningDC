009b764c811e5579bc2b63e5cd5ce99e94000dd2
Javad PourMostafa <javad.pourmostafa@gmail.com>
2018-04-02 20:00:22 +0430

    Multiple wieghts updates

--- Dataset5.py
+++ Dataset5.py
@@ -1,8 +1,2 @@
-import numpy as np
-
-input_data = np.array([1, 2, 3])
-target = 0
-weights = np.array([0, 2, 1])
-learning_rate = 0.01
 
 
--- MultipleWeightsUpdates.py
+++ MultipleWeightsUpdates.py
@@ -1,35 +1,43 @@
 # Make multiple update
 # This network does not have any hidden layers, and it goes directly from the input (3 nodes) to
 # an output. Note that weights is a single array
-import Dataset5
-from sklearn.metrics import mean_squared_error
+import numpy as np
 import matplotlib.pyplot as plt
 
+
+input_data = np.array([1, 2, 3])
+target = 0
+weights = np.array([0, 2, 1])
 UpdateNum = 20
 mse_hist = []
 
 
-def get_slope(inputd, target, weights):
-    preds = (inputd * weights).sum()
-    error = preds - target
+def get_error(inputd, target_actual, weight):
+    preds = (inputd * weight).sum()
+    error = preds - target_actual
+    return error
+
+
+def get_slope(inputd, target_actual, weight):
+    error = get_error(inputd, target_actual, weight)
     out = 2 * error * inputd
     return out
 
 
 def get_mse(inputd, target, updated_weights):
-    preds = (inputd * updated_weights).sum()
-    out = mean_squared_error(preds, target)
-    return out
+    errors = get_error(inputd, target, updated_weights)
+    mseresult = np.mean(errors**2)
+    return mseresult
 
 
 # Iterate over the number of updates
 for i in range(UpdateNum):
     # Calculate the slope
-    slope = get_slope(Dataset5.input_data, Dataset5.target, Dataset5.weights)
+    slope = get_slope(input_data, target, weights)
     # Update the weights
-    UpdatedWeights = Dataset5.weights - (slope * Dataset5.learning_rate)
+    weights = weights - (slope * 0.01)
     # Calculate mse with new weights(Updated Weights)
-    mse = get_mse(Dataset5.input_data, Dataset5.target, UpdatedWeights)
+    mse = get_mse(input_data, target, weights)
     # Append the mse to mse_hist[]
     mse_hist.append(mse)
 
