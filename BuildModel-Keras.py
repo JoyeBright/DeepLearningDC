a9a55c5f2ce9040b85bafa01c85fe3c1d02eabd9
Javad PourMostafa <javad.pourmostafa@gmail.com>
2018-04-05 20:33:19 +0430

    Add Keras model

--- BuildModel-Keras.py
+++ BuildModel-Keras.py
@@ -5,3 +5,24 @@
 # In Pandas DataFrame, .describe() generates descriptive statistics that summarize the central tendency,
 # dispersion, shape of a dataset's distribution, exluding NaN (Not a number) values.
 
+import numpy as np
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+predictors = np.loadtxt('', delimiter=',')
+n_cols = predictors.shape[1]
+model = Sequential()
+# Add the first layer
+model.add(Dense(50, Activation="relu", input_shape=(n_cols,)))
+# Add the second layer
+model.add(Dense(32, Activation="relu"))
+# Add the output layer
+model.add(Dense(1))
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+# What is loss function of the method?
+# By Printing model.loss u can access its loss function
+# Fitting the model
+model.fit(predictors, target)
+
